0cf94aaea697042c3d2cd303662969353d028ca3
Don Voorhies <donvoorhies@gmail.com>
2021-03-09 15:03:50 +0100

    Update OAP_plugin.php

--- OAP_plugin.php
+++ OAP_plugin.php
@@ -205,11 +205,10 @@ function contactform_dequeue_scripts() {
 		}
     }
     if( ! $load_scripts ) {
-		wp_dequeue_script('contact-form-7');
-        wp_dequeue_script('wpcf7-recaptcha-js-extra');
+        	wp_dequeue_script('wpcf7-recaptcha-js-extra');
 		wp_dequeue_script('wpcf7-recaptcha');
 		wp_dequeue_script('google-recaptcha');
-        wp_dequeue_style('contact-form-7');		
+        	wp_dequeue_style('contact-form-7');		
     }
 }
 add_action( 'wp_enqueue_scripts', 'contactform_dequeue_scripts', 99 );
