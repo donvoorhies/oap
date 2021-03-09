e526d7ad73873314597e9dd1d6b9cf1922e0d68b
Don Voorhies <donvoorhies@gmail.com>
2021-03-09 15:32:23 +0100

    Update OAP_plugin.php

--- OAP_plugin.php
+++ OAP_plugin.php
@@ -129,37 +129,6 @@ Source: Brian Jackson: "How To Speed-up wordpress" (PDF)- kinsta.com
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-/*!
-Safely disable WP REST API 
-*/
-add_filter( 'rest_authentication_errors', function( $result ) {
-    // If a previous authentication check was applied,
-    // pass that result along without modification.
-    if ( true === $result || is_wp_error( $result ) ) {
-        return $result;
-    }
-    // No authentication has been performed yet.
-    // Return an error if user is not logged in.
-    if ( ! is_user_logged_in() ) {
-        return new WP_Error(
-            'rest_not_logged_in',
-            __( 'You are not currently logged in.' ),
-            array( 'status' => 401 )
-        );
-    }
-    // Our custom authentication check should have no effect
-    // on logged-in requests
-    return $result;
-});
-
-/*!
-Sources: 
-https://stackoverflow.com/questions/41191655/safely-disable-wp-rest-api
-https://developer.wordpress.org/rest-api/frequently-asked-questions/#can-i-disable-the-rest-api
-*/
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 /*! 
 Remove extra CSS that the 'Recent Comments' widget injects
 */
